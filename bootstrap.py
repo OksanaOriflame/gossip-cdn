4216b96fed688234049947d15b4d53ee9919934d
Daniil Gilmutdinov <fregire@mail.ru>
2023-01-16 22:50:42 +0500

    changed starting of bootstrap node

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,17 @@
 from nodes.bootstrap_node import BootstrapNode
+from argparse import ArgumentParser
 
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument('--port', type=int, default=3333)
+
+    return parser.parse_args()
 
 def main():
-    node = BootstrapNode(3333)
+    args = parse_args()
+
+    node = BootstrapNode(args.port)
 
     try:
         node.start()
